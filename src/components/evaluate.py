f60c4ded116bc02f1e20192501a78a9d0e7dd271
snehangshu2002 <snehangshubhuin@gmail.com>
2026-06-10 18:46:24 +0530

    feat(evaluate): refactor evaluation process to utilize parameters from config and enhance logging

--- src/components/evaluate.py
+++ src/components/evaluate.py
@@ -9,46 +9,41 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_a
 
 from src.logger_config import get_logger
 from src.exception import CustomException
+from src.utils import load_params
+
+params = load_params()
 
 logger = get_logger( os.path.splitext(os.path.basename(__file__))[0])
 
-CATEGORICAL_COLS = ["Gender", "Customer Type", "Type of Travel", "Class"]
 
-RATING_COLS = [
-    'Departure and Arrival Time Convenience', 'Ease of Online Booking',
-    'Check-in Service', 'Online Boarding', 'Gate Location',
-    'On-board Service', 'Seat Comfort', 'Leg Room Service',
-    'Cleanliness', 'Food and Drink', 'In-flight Service',
-    'In-flight Wifi Service', 'In-flight Entertainment', 'Baggage Handling'
-]
 
-def load_model(file_path:str):
-    """Load trained model from a file"""
-    try:
-        logger.info("Model loading Started")
-        with open (file_path,'rb') as file:
-            model = joblib.load(file)
-            logger.debug("Model loaded Successful")
+# def load_model(file_path:str):
+#     """Load trained model from a file"""
+#     try:
+#         logger.info("Model loading Started")
+#         with open (file_path,'rb') as file:
+#             model = joblib.load(file)
+#             logger.debug("Model loaded Successful")
             
-            return model
-    except Exception as e:
-        CustomException(e,sys)
+#             return model
+#     except Exception as e:
+#         CustomException(e,sys)
 
 
-def load_data(file_path:str)->pd.DataFrame:
-    """
-    Load data from a CSV file.
+# def load_data(file_path:str)->pd.DataFrame:
+#     """
+#     Load data from a CSV file.
     
-    :param file_path: Path to the CSV file
-    :return: Loaded DataFrame
-    """
-    try:
-        logger.info("Load the data")
-        df=pd.read_csv(file_path)
-        logger.debug(f"data loaded from {file_path} with shape {df.shape}")
-        return df
-    except Exception as e:
-        raise CustomException(e,sys)
+#     :param file_path: Path to the CSV file
+#     :return: Loaded DataFrame
+#     """
+#     try:
+#         logger.info("Load the data")
+#         df=pd.read_csv(file_path)
+#         logger.debug(f"data loaded from {file_path} with shape {df.shape}")
+#         return df
+#     except Exception as e:
+#         raise CustomException(e,sys)
 
 def evaluate_model(model,X_test:np.ndarray,y_test:np.ndarray)->dict:
      """Evaluate the model and return the evaluation metrics."""
@@ -71,7 +66,10 @@ def evaluate_model(model,X_test:np.ndarray,y_test:np.ndarray)->dict:
              "recall":recall,
              "auc":auc
          }
+         
          logger.debug('Model evaluation metrics calculated')
+         return metrics_dict
+     
      except Exception as e:
          CustomException(e,sys)
 
@@ -89,41 +87,51 @@ def save_metrics(metrics: dict, file_path: str) -> None:
         
 def main():
     try:
-        # Load all objects
-        MODEL_DIR = Path(__file__).parent/"models"
         
-        encoder = joblib.load(MODEL_DIR/"encoder.pkl")
+        categorical_cols = params["features"]["categorical_cols"]
+        rating_cols = params["features"]["rating_cols"]
+        target_col = params["features"]["target_col"]
+        
+        encoder = joblib.load(params["artifacts"]["encoder_path"])
         logger.debug("Encoder saved object loaded")
-        scaler = joblib.load(MODEL_DIR / "scaler.pkl")
+        scaler = joblib.load(params["artifacts"]["scaler_path"])
         logger.debug("Scaler  saved object loaded")
-        label_encoder = joblib.load(MODEL_DIR / "label_encoder.pkl")
+        label_encoder = joblib.load(params["artifacts"]["label_encoder_path"])
         logger.debug("Label_encoder saved object loaded")
         
-        rating_medians = joblib.load(MODEL_DIR / "rating_medians.pkl")
+        rating_medians = joblib.load(params["artifacts"]["rating_medians_path"])
 
-        model = joblib.load(MODEL_DIR / "xgboost.pkl")
+        model = joblib.load(params["artifacts"]["model_path"])
         
-        test_data = load_data("data\processed\test.csv")
-        X_test = test_data.drop(columns=['Satisfaction'])
-        y_test = test_data['Satisfaction']
+        logger.info("All artifacts loaded")
         
-        for col in RATING_COLS:
+        test_data = pd.read_csv(params["data"]["processed_test_path"])
+        X_test = test_data.drop(columns=[target_col])
+        y_test = test_data[target_col]
+        
+        for col in rating_cols:
             if X_test[col].iloc[0]==0:
                 X_test[col]=rating_medians[col]
-        encoded = encoder.transform(X_test[CATEGORICAL_COLS])
+        encoded = encoder.transform(X_test[categorical_cols])
         encoded_X_test = pd.DataFrame(
             encoded,
             columns=encoder.get_feature_names_out(),
             dtype=int
         )
         # Drop categorical cols  and concat enocode  
-        X_test=X_test.drop(columns=CATEGORICAL_COLS)
+        X_test=X_test.drop(columns=categorical_cols)
         X_test = pd.concat([X_test,encoded_X_test],axis=1)
         
         X_test = scaler.transform(X_test)
         
         metrics = evaluate_model(model,X_test,y_test)
-        save_metrics(metrics,'reports/metrics.json')
-        
+        save_metrics(metrics,params["evaluate"]["metrics_path"])
+        logger.info(f"Evaluation complete: {metrics}")
     except Exception as e:
         CustomException(e,sys)
+        
+if __name__ == "__main__":
+    try:
+        main()
+    except CustomException:
+        sys.exit(1)
