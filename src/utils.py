7d85e01b586aee0cefca05d17a61189d5b3f6c99
snehangshu2002 <snehangshubhuin@gmail.com>
2026-06-13 17:21:22 +0530

    refactor: reorganize imports and improve code formatting in utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,30 +1,35 @@
-import yaml
-from src.logger_config import get_logger
-from src.exception import CustomException
-
-from dotenv import load_dotenv
 import os
-import mlflow
-load_dotenv()
 import sys
 
+import mlflow
+import yaml
+from dotenv import load_dotenv
+
+from src.exception import CustomException
+from src.logger_config import get_logger
+
+load_dotenv()
+
 logger = get_logger("Utils")
-def load_params(path:str="params.yaml")->dict:
+
+
+def load_params(path: str = "params.yaml") -> dict:
     """Load params.yaml and return as dict"""
-    
+
     try:
-        with open(path,"r") as f:
+        with open(path, "r") as f:
             params = yaml.safe_load(f)
         logger.debug(f"Params loaded from {path}")
         return params
     except Exception as e:
-        CustomException(e,sys)
+        CustomException(e, sys)
+
 
 def setup_mlflow(experiment_name: str) -> None:
     """Configure MLflow to track on DagsHub."""
     tracking_uri = os.environ.get("MLFLOW_TRACKING_URI")
-    username     = os.environ.get("MLFLOW_TRACKING_USERNAME")
-    password     = os.environ.get("MLFLOW_TRACKING_PASSWORD")
+    username = os.environ.get("MLFLOW_TRACKING_USERNAME")
+    password = os.environ.get("MLFLOW_TRACKING_PASSWORD")
 
     if not tracking_uri:
         raise EnvironmentError("MLFLOW_TRACKING_URI not set in .env")
